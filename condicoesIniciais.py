7d71dcac8177dc816a7d190f1e715a734d889d90
Potalej <oapotalej@gmail.com>
2022-11-07 12:32:08 -0300

    Adicionado gerador de posições e momentos com energia total nula.

---
+++ condicoesIniciais.py
@@ -0,0 +1,51 @@
+"""
+    Algumas condições iniciais que podem ser convenientes
+"""
+from math import sqrt
+from calculos.hamiltoniano import *
+import random
+
+class condicoesIniciais:
+    def __init__ (self, massas:list=[]):
+        self.massas = massas
+        self.qntdCorpos = len(massas)
+
+    def posicoes (self, intervalo:list, distancia_minima:float, inteiros:bool=True):
+        a,b = intervalo
+        x = random.sample(range(a, b, distancia_minima), self.qntdCorpos)
+        y = random.sample(range(a, b, distancia_minima), self.qntdCorpos)
+        return x, y
+    
+    def momentos (self, intervalo:list, inteiros:bool=True):
+        a,b = intervalo
+        x = [random.randrange(a, b) for i in range(self.qntdCorpos)]
+        y = [random.randrange(a, b) for i in range(self.qntdCorpos)]
+        return x, y
+
+    def juntar_ps (self, x, y, px, py):
+        ps = []
+        for i in range(self.qntdCorpos):
+            ps += [x[i], px[i], y[i], py[i]]
+        return ps
+    
+    def energia_nula (self, intervalo_posicao:list, intervalo_momento:list):
+        x,y = self.posicoes(intervalo_posicao[:2], intervalo_posicao[-1])
+        px,py = self.momentos(intervalo_momento)
+        ps = self.juntar_ps(x,y,px,py)
+
+        fator = sqrt(-U(ps, self.massas)/EC(ps, self.massas))
+        px = [pxi*fator for pxi in px]
+        py = [pyi*fator for pyi in py]
+        
+        R = [[x[i], y[i]] for i in range(self.qntdCorpos)]
+        P = [[px[i], py[i]] for i in range(self.qntdCorpos)]
+
+        ps = self.juntar_ps(x,y,px,py)
+        print(H(ps, self.massas))
+
+        return R, P
+
+
+# massas = [500 for i in range(25)]
+# x = random.sample(range(-300, 300, 10), 25)
+# y = random.sample(range(-300, 300, 10), 25)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from animacao.animacao import Animacao
+# # from animacao.animacao import Animacao
 from animacao.simular import Simulacao
 
 # m = [120, 200, 100]
@@ -6,8 +6,14 @@ from animacao.simular import Simulacao
 # P0 = [[0,0], [0,0], [0,0]]
 
 # S = Simulacao(m, R0, P0)
-# S.simular(False, True, 1000)
+# S.simular(exibir=True, salvar=False)
 
-from animacao.animacao import Animacao
+# # from animacao.animacao import Animacao
+# # Animacao().animar_salvo('pontos/pontos.txt')
 
-Animacao().animar_salvo('pontos/pontos.txt')
+from condicoesIniciais import condicoesIniciais
+
+m = [120, 200, 100]
+R0, P0 = condicoesIniciais(m).energia_nula([-100, 100, 30], [-1, 1])
+S = Simulacao(m, R0, P0)
+S.simular(exibir=True, salvar=False)

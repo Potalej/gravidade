a338025cd946e0998a844714b05ca76da13ef39c
Potalej <oapotalej@gmail.com>
2022-11-19 18:46:02 -0300

    Uma pequena melhora no desempenho do RK4 adaptado.

--- calculos/integracao.py
+++ calculos/integracao.py
@@ -3,6 +3,7 @@
 """
 from numpy import array, transpose, identity, einsum, true_divide, ones
 from calculos.colisoes import verificaColisoes
+from time import time
 
 class RK4:
     def __init__ (self, m:list, f:list, h:float=0.05):
@@ -10,10 +11,12 @@ class RK4:
         self.m = m
         self.guardar_massas()
         self.f = f # funções
-        self.A = [[], array([1/2]), array([0,1/2]), array([0,0,1])]
-        self.B = array([0,1/2,1/2,1])
-        self.C = array([0,1/2,1/2,1])
         self.h = h
+        self.A = [array([]), array([1/2]), array([0,1/2]), array([0,0,1])]
+        self.A = [Ai * h for Ai in self.A]
+        self.B = self.h * array([0,1/2,1/2,1])
+        self.C = self.h * array([0,1/2,1/2,1])
+        self.tempork4 = []
 
     def guardar_massas (self):
         """Armazena os produtos das massas em uma matriz"""
@@ -44,16 +47,25 @@ class RK4:
         yn1 = []
         for indice, fi in enumerate(self.f):
             kappas = []
-            for i in range(4):
-                kappa = yn + self.h * einsum('i,i', self.A[i], kappas)
-                kappas.append(fi(tn+self.C[i]*self.h, F, *kappa))
-            y1 = yn[indice] + self.h * einsum('i,i', array(kappas), self.B)
+            tk = time()
+            kappas.append(fi(tn+self.C[0]*self.h, F, *yn))
+            for i in range(1,4):
+                ein = einsum('i,i', self.A[i], array(kappas))
+                kappa = yn + ein
+                kappas.append(fi(tn+self.C[i], F, *kappa))
+            y1 = yn[indice] + einsum('i,i', array(kappas), self.B)
             yn1.append(y1)
+            self.tempork4.append(time() - tk)
         return tn + self.h, yn1
 
     def aplicarNVezes (self, tn:float, yn:list, n=1):
         for _ in range(n):
             F = self.forcas(yn)
+
             tn, yn = self.runge_kutta4(tn, yn, F)
+
+            
             yn = verificaColisoes(self.m, yn)
+            
+
         return tn, yn, F
